340ca60fdf6cdf032b637d74d5fbf8455610c001
Dariusz Skrzecinski <dariusz.skrzecinski@gmail.com>
2013-04-23 00:06:39 +0200

    Symfony ver: 1.04 AppCache i bootstrap zmienione

--- app/AppCache.php
+++ app/AppCache.php
@@ -5,5 +5,9 @@ require_once __DIR__.'/AppKernel.php';
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
+
+
+//TAKI jebutny test
+//Ciekawe jak to wyjdzie "Jeszcze raz"
 {
 }
